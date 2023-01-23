ba1c98970017ca6440308a6f78d72b8676f67c0e
Lukas-Vorndamme <90843192+Lukas-Vorndamme@users.noreply.github.com>
2023-01-23 13:09:16 +0100

    1 file modified

--- src/app/services/to-do-data.service.ts
+++ src/app/services/to-do-data.service.ts
@@ -3,26 +3,23 @@ import { ToDo } from '../model/ToDo';
 
 @Injectable()
 export class ToDoDataService {
+  todos: ToDo[] = [];
 
-  todos : ToDo[] = [];
-
-  constructor() { 
+  constructor() {
     this.todos.push(new ToDo('Einkaufen', false));
     this.todos.push(new ToDo('Fleisch pökeln', true, '28.12.2022'));
     this.todos.push(new ToDo('Hinterm Bahnhof prügeln', true));
     this.todos.push(new ToDo('Essen', false, '18.02.2023'));
     this.todos.push(new ToDo('Geschenke kaufen', true, '14.12.2022'));
-    this.todos.push(new ToDo('Drogen Kaufen',true,'23.01.2023'));
-    this.todos.push(new ToDo('Drogen Konsumieren',false,'24.01.2023'));
-  };
-saveTodo(todo : ToDo){
+    this.todos.push(new ToDo('Drogen Kaufen', true, '23.01.2023'));
+    this.todos.push(new ToDo('Drogen Konsumieren', false, '24.01.2023'));
+  }
+  saveTodo(todo: ToDo) {
+    this.todos.push(todo);
+  }
 
-  this.todos.push(todo);
-
-};
-
-toggleToDo(todo : ToDo){
-todo.done = !todo.done;
-this.todos = this.todos.filter(t => t = t)
-};
+  toggleToDo(todo: ToDo) {
+    todo.done = !todo.done;
+    this.todos = this.todos.filter((t) => (t = t));
+  }
 }
